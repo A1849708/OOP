9af52b6f8cbc5f90be8ec3fa14e3f69bf98a15d5
A1849708 <mathewalex2004@gmail.com>
2022-07-29 23:52:48 +0930

    very final commit

--- main-3-2.cpp
+++ main-3-2.cpp
@@ -4,6 +4,6 @@ extern int median_array(int array[], int n);
 
 int main(void){
     int array[8]={19, 20, 16, 5, 8, 1, 7, 11};
-    std::cout<<"the number is: "<<median_array(array,5)<<std::endl;
+    std::cout<<"the number is: "<<median_array(array,8)<<std::endl;
     return 0;
 }
