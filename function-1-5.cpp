e29d6f9f5b370eb4f21b473e07ba39dac0507b12
A1849708 <mathewalex2004@gmail.com>
2022-08-03 00:10:29 +0930

    commit 7

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -12,7 +12,7 @@ void print_summed(int array1[3][3], int array2[3][3]){
     
     for (int i=0;i<3;i++){
         for (int j=0;j<3;j++){
-            cout<<array1[i][j];
+            cout<<array1[i][j]<<" ";
         }
         cout<<endl;
     }
