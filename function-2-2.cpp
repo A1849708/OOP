5bebda8304e15396b68c17ebab55cf4bbafeb131
A1849708 <mathewalex2004@gmail.com>
2022-08-02 23:37:16 +0930

    commit 4 practical

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -5,14 +5,15 @@ using namespace std;
 
 int binary_to_int(int binary_digits[], int number_of_digits){
     int sum=0;
+    int count=number_of_digits-1;
     for (int i=0;i<number_of_digits;i++){
-        if (binary_digits[i]==1){
-            sum=sum+(binary_digits[i]*pow(2,(number_of_digits-1)));
-            number_of_digits--;
-        }
-        else{
-            number_of_digits--;
-        }
+        //if (binary_digits[i]==1){
+            sum=sum+(binary_digits[i]*pow(2,(count)));
+            count--;
+        //}
+        //else{
+        //    number_of_digits--;
+        //}
     }
     return sum;
 }
