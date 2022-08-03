faddf715f5d1d12826ebe0898b7128e6ff87c06c
A1849708 <mathewalex2004@gmail.com>
2022-08-03 12:14:43 +0930

    commit 9

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -13,7 +13,7 @@ void print_scaled(int array[3][3], int scale){
     for (int i=0;i<3;i++){
         for (int j=0;j<3;j++){
             cout<<array[i][j];
-            if (j!=3){
+            if (j!=2){
                 cout<<" ";
             }
         }
--- function-1-5.cpp
+++ function-1-5.cpp
@@ -13,7 +13,7 @@ void print_summed(int array1[3][3], int array2[3][3]){
     for (int i=0;i<3;i++){
         for (int j=0;j<3;j++){
             cout<<array1[i][j];
-            if (j!=3){
+            if (j!=2){
                 cout<<" ";
             }
         }
