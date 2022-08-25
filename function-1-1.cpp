9bbe19befbff5c052c8dd1e648a7f4f29bf799e5
A1849708 <mathewalex2004@gmail.com>
2022-08-25 17:50:15 +0930

    commit 6

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -4,17 +4,20 @@ using namespace std;
 
 void print_ascending(int *vals, int len){
     int count=*vals;
-    cout<<count<<" ";
+    cout<<count;
+        if (*(vals+1)>=count){
+        cout<<"*";
+    }
     for (int i=1;i<len;i++){
         if (*(vals+i)>=count){
             cout<<*(vals+i);
             count=*(vals+i);
+            if (*(vals+i+1)>=count){
+                cout<<"*";
+            }
         }
         else{
             break;
-        }
-        if (*(vals+i)>=count){
-            cout<<"*";
         }
     }
 }
